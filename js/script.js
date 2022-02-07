308288d4ac55f6648d9b26bfe6544abf6da09521
Adner16 <giovanni.renda16@gmail.com>
2022-02-07 18:42:31 +0100

    adde script

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 console.log('js ok');
 console.log('Vue ok', Vue);
+console.log('axios ok', axios);
 
 Vue.config.devtools = true;
 
 const app = new Vue({
     el: '#root', 
+    data:{
+        emails:[],
+        mailNum: 10,
+    },
+    methods: {
+        getRandomMail(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( res => {
+            const email = res.data.response;
+            this.emails.push(email);
+            })
+        }
+    },  
+    mounted(){
+        for(let i = 0; i < this.mailNum; i++){
+            this.getRandomMail();
+        }
+    }   
 })
